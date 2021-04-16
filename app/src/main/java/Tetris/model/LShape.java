5836e2b4f51d67a8d95ab5eed0f5ecf6d0eca9ec
Caroline Murphy <murphycb@slu.edu>
2021-04-16 09:40:54 -0500

    Updated version of LShape

--- app/src/main/java/Tetris/model/LShape.java
+++ app/src/main/java/Tetris/model/LShape.java
@@ -1,7 +1,8 @@
 package model;
 
 import java.awt.Color;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LShape extends TetrisShape
 {
@@ -10,26 +11,16 @@ public class LShape extends TetrisShape
 
      public LShape(Color color)
      {
-	this.color = color;
-
-	//Initialize each element with an arraylist
-	blockArray.setUpArray(3);
+	 super(color);
+	 this.color = color;
 
 	//set up initial L shaped block using array
-	blockArray.add(0, new Block(color));
-	blockArray.add(1, new Block(color));
-	blockArray.add(2, new Block(color));
-	blockArray.get(2).add(1, new Block(color));
+	blockArray.add(0, new ArrayList<Block>(Arrays.asList(new Block(color))));
+	blockArray.add(1, new ArrayList<Block>(Arrays.asList(new Block(color))));
+	blockArray.add(2, new ArrayList<Block>(Arrays.asList(new Block(color), new Block(color))));
+
 
 	orientation = 1; 
-     }
-
-     public ArrayList<ArrayList<Block>> setUpArray(int row)
-     {
-	for(int i=0; i<row; i++)
-	{
-	    blockArray.add(new ArrayList<Block>());
-	}
      }
 
      public void moveHorizontally()
@@ -46,53 +37,43 @@ public class LShape extends TetrisShape
      {
 	blockArray.clear();
 
-	if(orientation.equals(1))
+	if(orientation == 1)
 	{
-	     blockArray.setUpArray(2);
-	     blockArray.add(0, new Block(color));
-	     blockArray.get(0).add(1, new Block(color));
-	     blockArray.get(0).add(2, new Block(color));
-	     blockArray.add(1, new Block(color));
+
+	     blockArray.add(0, new ArrayList<Block>(Arrays.asList(new Block(color), new Block(color), new Block(color))));
+	     blockArray.add(1, new ArrayList<Block>(Arrays.asList(new Block(color))));
 
 	     orientation = 2;
 	}
 
-	else if(orientation.equals(2))
+	else if(orientation == 2)
 	{
-	     blockArray.setUpArray(3);
-	     blockArray.add(0, new Block(color));
-	     blockArray.get(0).add(1, new Block(color));
-	     blockArray.add(1, null);
-	     blockArray.get(1).add(1, new Block(color));
-	     blockArray.add(2, null);
-	     blockArray.get(2).add(1, new Block(color));
+
+	     blockArray.add(0, new ArrayList<Block>(Arrays.asList(new Block(color), new Block(color))));
+	     blockArray.add(1, new ArrayList<Block>(Arrays.asList(null, new Block(color))));
+	     blockArray.add(2, new ArrayList<Block>(Arrays.asList(null, new Block(color))));
 
 	     orientation = 3;
 	}
 
-	else if(orientation.equals(2))
+	else if(orientation == 3)
 	{
-	     blockArray.setUpArray(2);
-	     blockArray.add(0, null);
-	     blockArray.get(0).add(1, null);
-	     blockArray.get(0).add(2, new Block(color));
-	     blockArray.add(1, new Block(color));
-	     blockArray.get(1),add(1, new Block(color));
-	     blockArray.get(1).add(2, new Block(color));
+	     blockArray.add(0, new ArrayList<Block>(Arrays.asList(null, null, new Block(color))));
+	     blockArray.add(1, new ArrayList<Block>(Arrays.asList(new Block(color), new Block(color), new Block(color))));
+
 
 	     orientation = 4;
 	}
 
-	else if(orientation.equals(4))
+	else if(orientation == 4)
 	{
-	     blockArray.setUpArray(3);
-	     blockArray.add(0, new Block(color));
-	     blockArray.add(1, new Block(color));
-	     blockArray.add(2, new Block(color));
-	     blockArray.get(2).add(1, new Block(color));
+	     blockArray.add(0, new ArrayList<Block>(Arrays.asList(new Block(color))));
+	     blockArray.add(1, new ArrayList<Block>(Arrays.asList(new Block(color))));
+	     blockArray.add(2, new ArrayList<Block>(Arrays.asList(new Block(color), new Block(color))));
+
 
 	     orientation = 1; 
-	}
+        }
 
     }
 }
