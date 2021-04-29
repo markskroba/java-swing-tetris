a60edb1d4a24fb08794fa8d02cd03c20b469a491
Caroline Murphy <murphycb@slu.edu>
2021-04-29 16:46:19 -0500

    Framework done. Just need to actually draw the figures

--- app/src/main/java/Tetris/view/NextFigurePanel.java
+++ app/src/main/java/Tetris/view/NextFigurePanel.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.*;
+
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
@@ -7,6 +9,7 @@ import javax.swing.border.Border;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
 
 public class NextFigurePanel extends JPanel
 {
@@ -20,12 +23,48 @@ public class NextFigurePanel extends JPanel
 		layout.setVgap(0);
 		this.setLayout(layout);
 
-		drawNextFigure();
+
 	}
 
-	private void drawNextFigure()
+	public void drawNextFigure(String shape, Color color)
 	{
-		System.out.println("Draws a next figure to be dropped");
+		ArrayList<ArrayList<Block>> nextShape = new ArrayList<ArrayList<Block>> ();
+		
+		//populate null arraylist
+		for(int i=0; i<4; i++)
+		{
+			for (int j=0; j<3; j++)
+			{
+				nextShape.get(i).add(j, new Block());
+			}
+		}
+
+		if(shape.equals("LShape"))
+		{
+			
+		}
+		else if(shape.equals("ZShape"))
+		{
+
+		}
+		else if(shape.equals("SShape"))
+		{
+
+		}
+		else if(shape.equals("Square"))
+		{
+
+		}
+		else if(shape.equals("TShape"))
+		{
+
+		}
+		else if(shape.equals("Straight"))
+		{
+
+		}
+
+
 	}
 }
 
