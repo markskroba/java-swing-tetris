b1546536db797290b302192b566f9ab8cb44ec26
Caroline Murphy <murphycb@slu.edu>
2021-04-30 10:24:49 -0500

    Tentative coding for the next figure panel

--- app/src/main/java/Tetris/view/NextFigurePanel.java
+++ app/src/main/java/Tetris/view/NextFigurePanel.java
@@ -13,6 +13,9 @@ import java.util.ArrayList;
 
 public class NextFigurePanel extends JPanel
 {
+
+	protected int length;
+
 	public NextFigurePanel(int length) 
 	{
 		this.setPreferredSize(new Dimension(length, length));
@@ -33,7 +36,7 @@ public class NextFigurePanel extends JPanel
 		//populate null arraylist
 		for(int i=0; i<4; i++)
 		{
-			for (int j=0; j<3; j++)
+			for (int j=0; j<4; j++)
 			{
 				nextShape.get(i).add(j, new Block());
 			}
@@ -41,29 +44,65 @@ public class NextFigurePanel extends JPanel
 
 		if(shape.equals("LShape"))
 		{
-			
+			nextShape.get(1).get(0).setColor(color);
+			nextShape.get(2).get(0).setColor(color);
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(1).get(2).setColor(color);
 		}
 		else if(shape.equals("ZShape"))
 		{
-
+			nextShape.get(1).get(0).setColor(color);
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(2).get(1).setColor(color);
+			nextShape.get(2).get(2).setColor(color);
 		}
 		else if(shape.equals("SShape"))
 		{
-
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(1).get(2).setColor(color);
+			nextShape.get(2).get(0).setColor(color);
+			nextShape.get(2).get(1).setColor(color);
 		}
 		else if(shape.equals("Square"))
 		{
-
+			nextShape.get(1).get(2).setColor(color);
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(2).get(2).setColor(color);
+			nextShape.get(2).get(1).setColor(color);
 		}
 		else if(shape.equals("TShape"))
 		{
-
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(2).get(0).setColor(color);
+			nextShape.get(2).get(1).setColor(color);
+			nextShape.get(2).get(2).setColor(color);
 		}
 		else if(shape.equals("Straight"))
 		{
-
+			nextShape.get(0).get(1).setColor(color);
+			nextShape.get(1).get(1).setColor(color);
+			nextShape.get(2).get(1).setColor(color);
+			nextShape.get(3).get(1).setColor(color);
 		}
-
+		
+		int side = length/4;
+		for (int i=0; i<4; i++)
+		{
+			for (int j=0; j<4; j++)
+			{
+				if(nextShape.get(i).get(j).getColor() != null)
+				{		
+					JPanel block = new JPanel();
+					block.setPreferredSize(new Dimension(side,side));
+					block.setBackground(color);
+					Border border = BorderFactory.createLineBorder(Color.BLACK, 3);
+					block.setBorder(border);
+					this.add(block);
+					this.revalidate();
+					this.repaint();
+				}
+			}
+		}
 
 	}
 }
