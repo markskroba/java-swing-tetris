3dc321a7c61318438ff94cd94990249559840f68
Caroline Murphy <caroline.murphy.1@health.slu.edu>
2021-05-02 16:06:12 -0500

    Vertical and horizontal movement now in TetrisShape class

--- app/src/main/java/Tetris/model/TetrisShape.java
+++ app/src/main/java/Tetris/model/TetrisShape.java
@@ -37,9 +37,95 @@ public abstract class TetrisShape extends TetrisArray
 	
 	public abstract void createFourthRotation();
 	
-	public abstract void moveVertically();
+	public void moveVertically()
+	{	
+		//Make sure that it can't move out of bounds by checking that the last row is empty
+		boolean canMove = true;
+		for(int j=0; j<numCols; j++)
+		{
+			if(this.blockArray.get(numRows-1).get(j).getColor() != null)
+			{
+				canMove = false;
+			}
+		}
+		
+		if (canMove == true)
+		{
+			for (int i=14; i>=0; i--)
+			{
+				for (int j=0; j<numCols; j++)
+		 		{
+					if(this.blockArray.get(i).get(j).getColor() != null)
+					{
+						this.blockArray.get(i).get(j).setColor(null);
+						this.blockArray.get(i+1).get(j).setColor(this.shapeColor);
+					}
+		 		}	
+		 	}
+		}
 
-	public abstract void moveHorizontally(int direction);
+	}
+
+	public void moveHorizontally(int direction)
+	{
+		boolean canMove = true;
+
+		//to move left
+		if(direction < 0)
+		{
+			for (int i=0; i<numRows; i++)
+			{
+				if(this.blockArray.get(i).get(0).getColor() != null)
+				{
+					canMove = false;
+				}
+			}
+
+			if (canMove == true)
+			{
+				for (int i=0; i<numRows; i++)
+				{
+					for (int j=1; j<numCols; j++)
+					{
+						if(this.blockArray.get(i).get(j).getColor() != null)
+						{
+							this.blockArray.get(i).get(j).setColor(null);
+							this.blockArray.get(i).get(j-1).setColor(this.shapeColor);
+						}
+					}
+				}
+			}
+
+		}
+		//to move right
+		else if(direction > 0)
+		{
+			for (int i=0; i<numRows; i++)
+			{
+				if(this.blockArray.get(i).get(numCols-1).getColor() != null)
+				{
+					canMove = false;
+				}
+			}
+
+			if (canMove == true)
+			{
+				for (int i=0; i<numRows; i++)
+				{
+					for (int j=numCols-2; j>=0; j--)
+					{
+						if(this.blockArray.get(i).get(j).getColor() != null)
+						{
+							this.blockArray.get(i).get(j).setColor(null);
+							this.blockArray.get(i).get(j+1).setColor(this.shapeColor);
+						}
+					}
+				}
+			}
+
+		}
+
+	}
 
 	public abstract void rotateShape();
 }
