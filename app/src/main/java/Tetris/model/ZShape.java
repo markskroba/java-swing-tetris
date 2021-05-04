d02f90b94cd71260371c51b2b023a6714140b54e
Caroline Murphy <murphycb@slu.edu>
2021-05-04 17:54:55 -0500

    Fixing ZShape rotation

--- app/src/main/java/Tetris/model/ZShape.java
+++ app/src/main/java/Tetris/model/ZShape.java
@@ -23,26 +23,24 @@ public class ZShape extends TetrisShape
 		int i = 0;
 		boolean located = false;
 
-		while (located == false)
+		while (!located && i<numRows)
 		{
-			for (int j=0; j<numCols-1; j++)
+			for (int j=0; j<numCols-3; j++)
 			{
 				if(blockArray.get(i).get(j).getColor() != null && blockArray.get(i).get(j+1).getColor() != null)
 				{
-					if(i!=0 && (blockArray.get(i+1).get(j+1).getColor() != null))
+					//|_
+					//  |
+					if(i!=0 && (blockArray.get(i-1).get(j).getColor() != null))
 					{
-						blockArray.get(i+1).get(j+1).setColor(null);
-						blockArray.get(i+1).get(j+2).setColor(null);
-						blockArray.get(i-1).get(j+1).setColor(shapeColor);
-						blockArray.get(i+1).get(j).setColor(shapeColor);
+						blockArray.get(i-1).get(j).setColor(null);
+						blockArray.get(i+1).get(j+2).setColor(shapeColor);
 						located = true;
 					}
-					else if (i!= 0 && j != 0)
+					else if (i!=0 && blockArray.get(i+1).get(j+2).getColor() != null)
 					{
-						blockArray.get(i-1).get(j+1).setColor(null);
-						blockArray.get(i+1).get(j).setColor(null);
-						blockArray.get(i+1).get(j+1).setColor(shapeColor);
-						blockArray.get(i+1).get(j+2).setColor(shapeColor);
+						blockArray.get(i+1).get(j+2).setColor(null);
+						blockArray.get(i-1).get(j).setColor(shapeColor);
 						located = true;
 					}
 					else
