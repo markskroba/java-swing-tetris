62ad39087c5b569dcd9bc9840a5b3de244339fd7
Mark Skroba <mskroba@slu.edu>
2021-05-04 02:35:19 -0500

    Implemented Straight rotations

--- app/src/main/java/Tetris/model/Straight.java
+++ app/src/main/java/Tetris/model/Straight.java
@@ -23,6 +23,55 @@ public class Straight extends TetrisShape
 
 	public void rotateShape()
 	{
+		int i = 0;
+		boolean located = false;
+
+		while (located == false)
+		{
+			for (int j = 0; j < numCols - 1; j++) {
+				// |
+				if (blockArray.get(i).get(j).getColor() != null && blockArray.get(i + 1).get(j).getColor() != null && blockArray.get(i + 2).get(j).getColor() != null && blockArray.get(i + 3).get(j).getColor() != null)
+				{
+				    if (j >= 1 && j <= 6 )
+				    {
+						blockArray.get(i).get(j).setColor(null);
+						blockArray.get(i + 2).get(j).setColor(null);
+						blockArray.get(i + 3).get(j).setColor(null);
+
+						blockArray.get(i + 1).get(j - 1).setColor(shapeColor);
+						blockArray.get(i + 1).get(j + 1).setColor(shapeColor);
+						blockArray.get(i + 1).get(j + 2).setColor(shapeColor);
+					}
+
+				    else if (j >= 1 && j == 7)
+					{
+						blockArray.get(i).get(j).setColor(null);
+						blockArray.get(i + 2).get(j).setColor(null);
+						blockArray.get(i + 3).get(j).setColor(null);
+
+						blockArray.get(i + 1).get(j - 2).setColor(shapeColor);
+						blockArray.get(i + 1).get(j - 1).setColor(shapeColor);
+						blockArray.get(i + 1).get(j + 1).setColor(shapeColor);
+					}
+
+					located = true;
+				}
+				// --
+				else if (blockArray.get(i).get(j).getColor() != null && blockArray.get(i).get(j+1).getColor() != null && blockArray.get(i).get(j+2).getColor() != null && blockArray.get(i).get(j+3).getColor() != null)
+				{
+				    if (blockArray.get(i+1).get(j+1).getColor() == null && blockArray.get(i+2).get(j+1).getColor() == null)
+						blockArray.get(i).get(j).setColor(null);
+						blockArray.get(i).get(j+2).setColor(null);
+						blockArray.get(i).get(j+3).setColor(null);
+
+						blockArray.get(i-1).get(j+1).setColor(shapeColor);
+						blockArray.get(i+1).get(j+1).setColor(shapeColor);
+						blockArray.get(i+2).get(j+1).setColor(shapeColor);
+						located = true;
+				}
+			}
+			i++;
+		}
 		
 	}
 }
