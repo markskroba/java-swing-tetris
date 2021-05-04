1df0769d58bf955626da0c9dd0b99e15578e0895
Mark Skroba <mskroba@slu.edu>
2021-05-04 02:35:36 -0500

    Implemented LShape rotations

--- app/src/main/java/Tetris/model/LShape.java
+++ app/src/main/java/Tetris/model/LShape.java
@@ -22,6 +22,71 @@ public class LShape extends TetrisShape
 
 	public void rotateShape()
 	{
+		int i = 0;
+		boolean located = false;
+
+		while (located == false)
+		{
+			for (int j = 0; j < numCols - 1; j++)
+			{
+			    {
+					if (blockArray.get(i).get(j).getColor() != null && blockArray.get(i).get(j + 1).getColor() != null && blockArray.get(i).get(j + 2).getColor() != null) {
+						if (blockArray.get(i + 1).get(j).getColor() != null) {
+							blockArray.get(i).get(j).setColor(null);
+							blockArray.get(i).get(j + 2).setColor(null);
+							blockArray.get(i + 1).get(j).setColor(null);
+
+							blockArray.get(i - 1).get(j + 1).setColor(shapeColor);
+							blockArray.get(i + 1).get(j + 1).setColor(shapeColor);
+							blockArray.get(i - 1).get(j).setColor(shapeColor);
+
+							located = true;
+						}
+						else if(blockArray.get(i-1).get(j+2).getColor() != null)
+						{
+							blockArray.get(i).get(j).setColor(null);
+							blockArray.get(i-1).get(j+2).setColor(null);
+							blockArray.get(i).get(j+2).setColor(null);
+							blockArray.get(i+1).get(j).setColor(null);
+
+							blockArray.get(i-1).get(j+1).setColor(shapeColor);
+							blockArray.get(i+1).get(j+1).setColor(shapeColor);
+							blockArray.get(i+1).get(j+2).setColor(shapeColor);
+
+							located = true;
+						}
+					} else if (blockArray.get(i).get(j).getColor() != null && blockArray.get(i + 1).get(j).getColor() != null && blockArray.get(i + 2).get(j).getColor() != null) {
+						if (blockArray.get(i).get(j - 1).getColor() != null) {
+							blockArray.get(i).get(j - 1).setColor(null);
+							blockArray.get(i).get(j).setColor(null);
+							blockArray.get(i + 2).get(j).setColor(null);
+
+							blockArray.get(i + 1).get(j - 1).setColor(shapeColor);
+							blockArray.get(i + 1).get(j + 1).setColor(shapeColor);
+							blockArray.get(i).get(j + 1).setColor(shapeColor);
+
+							located = true;
+						}
+						else if (blockArray.get(i+2).get(j+1).getColor() != null)
+						{
+							blockArray.get(i+2).get(j+1).setColor(null);
+							blockArray.get(i).get(j).setColor(null);
+							blockArray.get(i+2).get(j).setColor(null);
+
+							blockArray.get(i+1).get(j-1).setColor(shapeColor);
+							blockArray.get(i+1).get(j+1).setColor(shapeColor);
+							blockArray.get(i+2).get(j-1).setColor(shapeColor);
+
+							located = true;
+
+						}
+
+					}
+				}
+			}
+			i++;
+		}
+
 
 	}
 }
